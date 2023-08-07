e413014e152a6ec58954abade9026706e1504bdd
D <test@>
2023-08-07 22:10:14 +0600

    Returning simple JSON explicitly

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -5,10 +5,10 @@ import (
 	"net/http"
 )
 
-// Declare a handler which writes a plain-text response with information about the
-// application status, operating environment and version.
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+	js := `{"status": "available", "environment": %q, "version": %q}`
+	js = fmt.Sprintf(js, app.config.env, version)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(js))
 }
