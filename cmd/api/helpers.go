0ab91acc15a56f0495c1666f3c7c91259f4ad4af
D <test@>
2023-08-09 12:34:56 +0600

    Validating JSON

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -89,3 +89,7 @@ func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst int
 func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
 	app.errorResponse(w, r, http.StatusBadRequest, err.Error())
 }
+
+func (app *application) failedValidationResponse(w http.ResponseWriter, r *http.Request, errors map[string]string) {
+	app.errorResponse(w, r, http.StatusUnprocessableEntity, errors)
+}
