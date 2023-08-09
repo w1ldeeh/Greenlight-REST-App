81314346f396f95562de1c2aa631638496696b82
D <test@>
2023-08-09 09:20:29 +0600

    Handling unsupported routes, finish JSON

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -8,6 +8,8 @@ import (
 	"strconv"
 )
 
+type envelope map[string]interface{}
+
 func (app *application) readIDParam(r *http.Request) (int64, error) {
 	params := httprouter.ParamsFromContext(r.Context())
 	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
@@ -17,7 +19,7 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 	return id, nil
 }
 
-func (app *application) writeJSON(w http.ResponseWriter, status int, data interface{}, headers http.Header) error {
+func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
 	js, err := json.MarshalIndent(data, "", "\t")
 	if err != nil {
 		return err
@@ -32,5 +34,6 @@ func (app *application) writeJSON(w http.ResponseWriter, status int, data interf
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	w.Write(js)
+
 	return nil
 }
--- internal/data/movies.go
+++ internal/data/movies.go
@@ -9,7 +9,7 @@ type Movie struct {
 	CreatedAt time.Time `json:"-"` //`json:"created_at"`
 	Title     string    `json:"title"`
 	Year      int32     `json:"year,omitempty"`           //`json:"year"`
-	Runtime   int32     `json:"runtime,omitempty,string"` //`json:"runtime"`
+	Runtime   Runtime   `json:"runtime,omitempty,string"` //`json:"runtime"`
 	Genres    []string  `json:"genres,omitempty"`         //`json:"genres"`
 	Version   int32     `json:"version"`
 }
